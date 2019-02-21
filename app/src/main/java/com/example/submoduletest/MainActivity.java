09ac72838958aa91375ad18e68ac10db499775cb
Max Nasalevich <maks20061999@gmail.com>
2019-02-21 15:18:06 +0300

    call function from submodule

--- app/src/main/java/com/example/submoduletest/MainActivity.java
+++ app/src/main/java/com/example/submoduletest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.submoduletest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.mylib.MyClass;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        MyClass.myFunc();
     }
 }
